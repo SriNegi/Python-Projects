ab2a9ff03289024b2142a1a1196c1e94560ea041
SriNegi <negisrishti27@gmail.com>
2026-02-20 18:33:25 +0530

    Added Number Guessing game and updated Readme

--- Number Guessing Game/guessgame.py
+++ Number Guessing Game/guessgame.py
@@ -1,1 +1,18 @@
+# Guess number 
+import random
+number_guess=random.randint(1,50)
+attempt=0
+user_guess=0
 
+print("Welcome to the Number Guessing Game! ")
+print("Guess a number between 1 and 50")
+
+while user_guess != number_guess:
+    user_guess=int(input())
+    attempt +=1
+    if user_guess>number_guess:
+        print("Too High Try Again")
+    elif user_guess<number_guess:
+         print("Too Low Try Again")
+    else :
+        print(f"Congratulations you have guessed the number correct in {attempt} attempts")
--- README.md
+++ README.md
@@ -10,6 +10,16 @@ This is a simple command-line calculator built using Python.
 - Division
 - Exit option
 - Uses match-case (Python 3.10+)
+# 2 Number Guessing Game
+
+This is a simple number guessing game built using Python.
+
+## Features
+
+- Random number generation between 1 and 50
+- Tracks number of attempts
+- Hints if guess is too high or too low
+- Shows congratulations when guessed correctly
 
 ## How to Run
 1. Make sure Python 3.10+ is installed.
